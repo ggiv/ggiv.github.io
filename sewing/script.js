f37fb1601c606471e0911cf684c48918a78326ce
ggiv <ggeorgiev.xxx@gmail.com>
2017-08-18 14:58:10 +0300

    Add files via upload

---
+++ sewing/script.js
@@ -0,0 +1,5 @@
+let modal_message= document.querySelector('#modalMessage')
+let button = document.querySelector('#downloadButton1')
+button.addEventListener('click',()=>{
+		modal_message.classList.toggle('modal-hidden');
+	});
