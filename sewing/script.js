da7b2b25e4c26f073b7afadef70eeae0173039a3
ggiv <ggeorgiev.xxx@gmail.com>
2018-05-21 17:34:56 +0300

    Delete script.js

--- sewing/script.js
+++
@@ -1,5 +0,0 @@
-let modal_message= document.querySelector('#modalMessage')
-let button = document.querySelector('#downloadButton1')
-button.addEventListener('click',()=>{
-		modal_message.classList.toggle('modal-hidden');
-	});
